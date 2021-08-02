8cabeab740e5c6b605c1bc761456eddbfe0a9e4d
saumya-mendiratta <saya.mendiratta@gmail.com>
2021-08-03 01:50:46 +0530

    Create Q215.java

---
+++ Q215.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+import java.util.Arrays;
+
+public class Q215 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = {3,2,3,1,2,4,5,5,6};
+		Arrays.sort(arr);
+		
+		for(int val : arr)
+			System.out.print(val + " " );
+	}
+
+}
