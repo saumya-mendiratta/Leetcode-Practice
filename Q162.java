c862116e31d042fd1b1ef2d152ec80ca4ba10625
saumya-mendiratta <saya.mendiratta@gmail.com>
2021-08-03 01:51:07 +0530

    Update Q162.java

--- Q162.java
+++ Q162.java
@@ -9,29 +9,30 @@ public class Q162 {
 
 	public int findPeakElement(int[] nums) {
 
-		int low = 0;
-		int high = nums.length - 1;
+		int lo = 0;
+		int hi = nums.length - 1;
 
-		int mid = (high + low) / 2;
+		while (lo <= hi) {
 
-		while (low <= high) {
+			int mid = (lo + hi) / 2;
 
-			if (mid < nums.length - 1 && nums[mid + 1] > nums[mid]) {
-				low = mid + 1;
-			} else if (mid > 0 && nums[mid - 1] > nums[mid]) {
-				high = mid - 1;
-			} else if (mid < nums.length - 1 && mid > 0 && nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+			// BASE case for n-1th element
+			if (mid == nums.length - 1)
 				return mid;
-			}
 
+			// case 4
+			if (mid >= 1 && mid <= nums.length - 1 && nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+				return mid;
+			// case 1
+			else if (nums[mid + 1] > nums[mid])
+				lo = mid + 1;
+			// case 2 & 3
 			else
-				return mid;
+				hi = mid - 1;
 
-			mid = (high + low) / 2;
 		}
 
-		return -1;
-
+		return 0;
 	}
 
 }
